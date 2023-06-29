f62892386fb418c22f3353736d4cae42aff891a1
NiKoBoSTiCk <salvatore.romanello@hotmail.it>
2023-06-29 12:35:17 +0200

    fixed DocumentService

--- src/app/services/document/document.service.ts
+++ src/app/services/document/document.service.ts
@@ -11,8 +11,7 @@ import {TokenStorageService} from "../token-storage/token-storage.service";
   providedIn: 'root'
 })
 export class DocumentService {
-
-  private documentUrl = 'http://172.28.175.170:4000/api/documents';
+  private documentUrl: string = 'http://172.28.175.170:4000/api/documents';
 
   constructor(
     private http: HttpClient,
@@ -37,56 +36,67 @@ export class DocumentService {
     );
   }
 
-  updateDocument(docInfo: DocumentInfo, docFile?: DocumentFile): Observable<any> {
+  updateDocument(docInfo: DocumentInfo, docFile?: File): Observable<any> {
     const data: FormData = new FormData();
-    data.append("docInfo", JSON.stringify(docInfo))
+    data.append("docInfo", new Blob([JSON.stringify(docInfo)], {type: "application/json"}));
     if (docFile != null)
-      data.append("docFile", docFile.file, docFile.filename)
+      data.append("docFile", new Blob([docFile], {type: "multipart/form-data"}))
     return this.http.put(this.documentUrl, data, {
-      reportProgress: true,
-      observe: 'events'
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
     }).pipe(
       catchError(this.handleError('updateDocument'))
     );
   }
 
   getDocument(id: number): Observable<DocumentFile> {
-    return this.http.get<DocumentFile>(this.documentUrl + "?id=" + id).pipe(
+    return this.http.get<DocumentFile>(this.documentUrl + "?id=" + id, {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
+    }).pipe(
       catchError(this.handleError<DocumentFile>('getDocument'))
     );
   }
 
   getReport(year: number): Observable<any> {
-    return this.http.get(this.documentUrl + "/report" + "?year=" + year).pipe(
+    return this.http.get(this.documentUrl + "/report" + "?year=" + year, {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
+    }).pipe(
       catchError(this.handleError('getReport', []))
     );
   }
 
   getAllDocuments(): Observable<DocumentInfo[]> {
-    return this.http.get<DocumentInfo[]>(this.documentUrl + "/all").pipe(
+    return this.http.get<DocumentInfo[]>(this.documentUrl + "/all", {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
+    }).pipe(
       catchError(this.handleError<DocumentInfo[]>('getAllDocuments', []))
     );
   }
 
   getDocumentsByTag(tag: string): Observable<DocumentInfo[]> {
-    return this.http.get<DocumentInfo[]>(this.documentUrl + "/by_tag?tag=" + tag).pipe(
+    return this.http.get<DocumentInfo[]>(this.documentUrl + "/by_tag?tag=" + tag, {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
+    }).pipe(
       catchError(this.handleError<DocumentInfo[]>('getDocumentsByTag', []))
     );
   }
 
   getDocumentsByYear(year: number): Observable<DocumentInfo[]> {
-    return this.http.get<DocumentInfo[]>(this.documentUrl + "/by_year?year=" + year).pipe(
+    return this.http.get<DocumentInfo[]>(this.documentUrl + "/by_year?year=" + year, {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
+    }).pipe(
       catchError(this.handleError<DocumentInfo[]>('getAllDocumentsByYear', []))
     );
   }
 
   getDocumentsByYearAndTag(year: number, tag: string): Observable<DocumentInfo[]> {
-    return this.http.get<DocumentInfo[]>(this.documentUrl + "/by_year_and_tag?year=" + year + "?tag=" + tag).pipe(
+    return this.http.get<DocumentInfo[]>(this.documentUrl + "/by_year_and_tag?year=" + year + "?tag=" + tag, {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenStorageService.getToken()})
+    }).pipe(
       catchError(this.handleError<DocumentInfo[]>('getAllDocumentsByYearAndTag', []))
     );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
+  private handleError<T>(operation: string = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       this.log(`${operation} failed: ${error.message}`);
@@ -94,7 +104,7 @@ export class DocumentService {
     };
   }
 
-  private log(message: string) {
+  private log(message: string): void {
     this.messageService.add(`DocumentService: ${message}`);
   }
 }
